8fcf40d97dece65e39fd9e97aed07983bfcf13f5
filipebotti <filipebotti@hotmail.com>
2018-10-23 09:18:05 -0300

    Fix db cfg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_22_200222) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
