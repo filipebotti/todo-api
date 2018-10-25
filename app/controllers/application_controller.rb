cd4ba90a6772a78d3b6e2ba4fc9cdc64377b8954
filipebotti <filipebotti@hotmail.com>
2018-10-25 17:48:01 -0300

    add cors headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::API
+
+	before_action :add_cors_headers
 	before_action :auth_validation
+	
+	def add_cors_headers
+		origin = request.headers["Origin"]		
+		headers['Access-Control-Allow-Origin'] = origin
+		headers['Access-Control-Allow-Methods'] = 'POST, GET, PATCH, PUT, DELETE'
+		allow_headers = request.headers["Access-Control-Request-Headers"]
+		if allow_headers.nil?
+			#shouldn't happen, but better be safe
+			allow_headers = 'Origin, Authorization, Accept, Content-Type'
+		end
+		headers['Access-Control-Allow-Headers'] = allow_headers		
+		headers['Access-Control-Max-Age'] = '1728000'
+	end
 
 	def auth_validation
 		if request.headers['Authorization'].present?
