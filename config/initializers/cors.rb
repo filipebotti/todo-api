02fcca8814d0deeacea1554b37376083c70cc4f7
filipebotti <filipebotti@hotmail.com>
2018-10-25 17:37:27 -0300

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
-
-  resource '*',
-    headers: :any,
-    expose: ['Jwt-Token-Renewed'],
-    methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
+    origins '*'
+    resource '*',
+      headers: :any,
+      expose: ['Jwt-Token-Renewed'],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end  
 end
